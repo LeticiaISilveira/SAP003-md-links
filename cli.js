666213f3301d6296f001768452783efa6a0e88ac
Leticia <leticia.ingrid.silveira@gmail.com>
2019-11-30 14:28:49 -0300

    read e regex  ok

---
+++ cli.js
@@ -0,0 +1,29 @@
+const mdLinks = require("./lib/index.js");
+
+mdLinks('README.md')
+  .then(links => console.log(links))
+  .catch(error => console.log('Error: ', error))
+  
+
+// mdLinks("./example.md")
+//   .then(links => {
+//     // => [{ href, text }]
+//   })
+//   .catch(console.error);
+// console.log(mdLinks())
+
+
+/*
+const mdLinks = require("./lib/mdLinks.js");
+
+mdLinks("./example.md")
+  .then(links => {
+    // => [{ href, text }]
+  })
+  .catch(console.error);*/
+
+  /*
+  const reg = /teste/
+
+
+  */
